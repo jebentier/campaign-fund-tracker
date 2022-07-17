b715c6fbf272f158c52672efb98460e5813132da
James Ebentier <jebentier@gmail.com>
2022-07-17 19:59:35 +0200

    Update graphql structure for querying

--- backend/graphql/query.rb
+++ backend/graphql/query.rb
@@ -6,14 +6,19 @@ require_relative 'types/contribution'
 require_relative 'types/candidate'
 
 class QueryType < GraphQL::Schema::Object
+  implements GraphQL::Types::Relay::Node
+
   description "The query root of this schema"
+
+  field :node,  Types::LegalEntityInterface, null: false do
+    argument :id, ID, required: true
+  end
 
   field :committees, [Types::Committee], null: false do
     description 'Get all commitees of the system'
 
     argument :first,  Integer, required: false, default_value: 10
     argument :offset, Integer, required: false, default_value: 0
-    argument :id,     ID,      required: false, default_value: nil
   end
 
   field :candidates, [Types::Candidate], null: false do
@@ -21,22 +26,17 @@ class QueryType < GraphQL::Schema::Object
 
     argument :first,  Integer, required: false, default_value: 10
     argument :offset, Integer, required: false, default_value: 0
-    argument :id,     ID,      required: false, default_value: nil
   end
 
-  def committees(first:, offset:, id:)
-    if id
-      GlobalID.find(id)
-    else
-      Committee.limit(first).offset(offset)
-    end
+  def node(id:)
+    GlobalID.find(id)
   end
 
-  def candidates(first:, offset:, id:)
-    if id
-      GlobalID.find(id)
-    else
-      Candidate.limit(first).offset(offset)
-    end
+  def committees(first:, offset:)
+    Committee.limit(first).offset(offset)
+  end
+
+  def candidates(first:, offset:)
+    Candidate.limit(first).offset(offset)
   end
 end
--- backend/graphql/types/base.rb
+++ backend/graphql/types/base.rb
@@ -11,21 +11,26 @@ module Types
     include GraphQL::Types::Relay::Node
 
     field :name,    String, null: false
+    field :type,    String, null: false
     field :state,   String, null: false
     field :country, String, null: false
 
     field :total_outbound_contributions, Float, null: false
     field :total_inbound_contributions,  Float, null: false
 
-    field :outbound_contributions,       Types::Contribution.connection_type, null: false
-    field :inbound_contributions,        Types::Contribution.connection_type, null: false
+    field :outbound_contributions, Types::Contribution.connection_type, null: false
+    field :inbound_contributions,  Types::Contribution.connection_type, null: false
+
+    def type
+      object.class.name
+    end
 
     def outbound_contributions
-      object.outbound_contributions
+      object.outbound_contributions.group(:destination_type, :destination_id).select('*, sum(amount) as amount').order(amount: :desc)
     end
 
     def inbound_contributions
-      object.inbound_contributions
+      object.inbound_contributions.group(:source_type, :source_id).select('*, sum(amount) as amount').order(amount: :desc)
     end
 
     def total_outbound_contributions
